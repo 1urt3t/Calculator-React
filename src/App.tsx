b1165b7710e7bc347de27004ff44d4d431734604
Alexsandr <lurt3t87@mail.ru>
2023-09-24 21:00:44 +0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+import Calculator from "./components/Calculator"
+import './App.css'
+export default function App() {
+  return (
+    <div className="App"><Calculator/></div>
+  )
+}
---
+++ src/components/Calculator.tsx
@@ -0,0 +1,10 @@
+
+
+export default function Calculator() {
+  return (
+    <div className="calculator">
+      <div id="first">Hello1</div> 
+      <div id="second">Hello2</div>
+    </div>
+  )
+}
